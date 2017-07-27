76bdbcf91de7eea7c4bcec4dea23140a3133b12b
yanbing <bing.yan@daigj.com>
2017-07-27 16:20:51 +0800

    over

--- one-piece/src/main.js
+++ one-piece/src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 import axios from 'axios'
 import Vuex from 'vuex'
 import store from './store'
 import Validator from 'vue-validator'
-
+import App from './App'
+Vue.use(ElementUI)
 Vue.use(Vuex);
 Vue.use(Validator);
 Vue.config.productionTip = false;
--- one-piece/src/pages/Home.vue
+++ one-piece/src/pages/Home.vue
@@ -1,71 +1,48 @@
 <template>
 	<div class="home-wrapper">
-		<div class="toast-wrapper" v-show="bShowToast"></div>
-		<div class="turn-table"></div>
-	<!-- <Calendar></Calendar> -->
-		<!-- <ArcProgress :config="config"></ArcProgress> -->
+		<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+		  <el-menu-item index="1">处理中心</el-menu-item>
+		  <el-submenu index="2">
+		    <template slot="title">我的工作台</template>
+		    <el-menu-item index="2-1">选项1</el-menu-item>
+		    <el-menu-item index="2-2">选项2</el-menu-item>
+		    <el-menu-item index="2-3">选项3</el-menu-item>
+		  </el-submenu>
+		  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+		</el-menu>
+		<div class="line"></div>
 	</div>
 </template>
 <script>
-	import Calendar from '../components/Calendar'
 	import * as util from '../utils'
 	import Api from '../api'
 	export default {
 		data() {
-			return {
-				bShowToast: false,
-				itemList: [],
-				// config: {
-				// 	start: 0,//起始弧度
-				// 	to: Math.PI,//结束弧度
-				// 	scale: Math.PI/90,//速度
-				// 	end: 0,//每次结束弧度
-				// 	total: 2 * Math.PI, 
-				// 	wdith: 400,
-				// 	height: 400,
-				// }
-			}
-		},
-		// beforeRouteEnter(to, from, next) {
-			
-		// },
-		created() {
-			// setTimeout(()=> {
-			// 	this.bShowToast = false;
-			// }, 1500);
-			// console.log(util.formatDate(423453245235))
-			// console.log(~~2)
-			// this.getInitData();
-
-		},
-		methods: {
-			getInitData() {
-				let _this = this;
-				_this.$axios.get('/imageList')
-				.then((response) => {
-					// console.log(response.data);
-					_this.itemList = response.data;
-				})
-			}
-		},
-		components: {
-			Calendar
-		}
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 	}
 </script>
 <style lang="stylus">
-	// @import ''
-	.toast-wrapper
-		position fixed 
-		height 100%
-		width 100%
-		background url(../assets/resource/index_toast.jpg)
-		background-repeat no-repeat
-		background-size cover
-	.turn-table 
-		height 200px
-		width 200px
-		background red	
-		margin 0 auto
-		margin-top 200px
+	// // @import ''
+	// .toast-wrapper
+	// 	position fixed 
+	// 	height 100%
+	// 	width 100%
+	// 	background url(../assets/resource/index_toast.jpg)
+	// 	background-repeat no-repeat
+	// 	background-size cover
+	// .turn-table 
+	// 	height 200px
+	// 	width 200px
+	// 	background red	
+	// 	margin 0 auto
+	// 	margin-top 200px
 </style>
